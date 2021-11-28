b3b096e3118998d1623d2070952531c47af5c3b5
hmallen <allenhm@gmail.com>
2021-11-28 04:50:12 -0600

    Detection, timing, and spray function complete.

--- BadKitty.ino
+++ BadKitty.ino
@@ -1,41 +1,70 @@
 #include <HCSR04.h>
 #include <Servo.h>
 
-#define SPRAY_POS 45
+#define DETECT_DIST_CM 100.0
+#define DETECT_TIMEOUT_SEC 10
+#define DETECT_COUNT_SPRAY 5
+#define SPRAY_POS 150
 #define SPRAY_DELAY 5
 
 const int triggerPin = 7;
 const int echoPin = 8;
 const int servoPin = 9;
+const int ledPin = 13;
+
+bool catDetected = false;
+unsigned int detectLast = 0;
+int detectCount = 0;
+int detectTimeout = DETECT_TIMEOUT_SEC * 1000;
 
 int pos = 0;
 
+UltraSonicDistanceSensor distSensor(triggerPin, echoPin);
 Servo sprayServo;
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial) {
-    delay(5);
-  }
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
 
-  HCSR04.begin(triggerPin, echoPin);
+  Serial.begin(115200);
+
+  delay(1000);
 
   sprayServo.attach(servoPin);
   sprayServo.write(90);
 
-  delay(1000);
+  for (int x = 0; x < 10; x++) {
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+  }
 }
 
 void loop() {
-  double* distCm = HCSR04.measureDistanceCm();
-
+  float distCm = distSensor.measureDistanceCm();
   Serial.print(F("distCm: "));
-  Serial.println(distCm[0]);
+  Serial.println(distCm);
 
-  spray();
-  while (true) {
-    delay(1000);
+  if (distCm <= DETECT_DIST_CM) {
+    detectLast = millis();
+    detectCount++;
+    digitalWrite(ledPin, LOW);
   }
+
+  if (detectCount == DETECT_COUNT_SPRAY) {
+    spray();
+    detectCount = 0;
+    digitalWrite(ledPin, HIGH);
+  }
+
+  else if ((millis() - detectLast) > detectTimeout) {
+    detectCount = 0;
+    digitalWrite(ledPin, HIGH);
+  }
+
+  Serial.print(F("detectCount: "));
+  Serial.println(detectCount);
 
   delay(1000);
 }
